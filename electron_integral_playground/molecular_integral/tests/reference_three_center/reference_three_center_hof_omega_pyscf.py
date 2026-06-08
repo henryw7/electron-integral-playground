b8116ebd7d323bca6ab84582d89b25c47cd73cdb
Henry Wang <henrywang0142857@gmail.com>
2026-06-08 16:37:54 -0700

    Complete J3c tests

---
+++ electron_integral_playground/molecular_integral/tests/reference_three_center/reference_three_center_hof_omega_pyscf.py
@@ -0,0 +1,22 @@
+
+from pyscf import gto, df
+import numpy as np
+
+mol = gto.M()
+mol.atom = """
+    H -1 0.1 0
+    O 0 0 0
+    F 0.5 0.6 0.7
+"""
+mol.basis = "sto-3g"
+omega = 0.2
+mol.set_range_coulomb(omega)
+mol.build()
+
+auxmol = df.addons.make_auxmol(mol, auxbasis = "def2-svp-ri")
+
+J3c = df.incore.aux_e2(mol, auxmol, 'int3c2e', aosym='s1', comp=1)
+J3c = J3c.transpose(2,0,1)
+
+np.set_printoptions(linewidth = np.iinfo(np.int32).max, threshold = np.iinfo(np.int32).max, precision = 16, suppress = True)
+np.savez_compressed('reference_three_center_hof_omega_data.npz', J3c = J3c)
--- electron_integral_playground/molecular_integral/tests/test_three_center_integral.py
+++ electron_integral_playground/molecular_integral/tests/test_three_center_integral.py
@@ -15,9 +15,9 @@ current_directory = pathlib.Path(__file__).parent.resolve()
 reference_directory = current_directory / "reference_three_center"
 
 pyscf_bohr_integral_threshold = 1e-12
-pyscf_angstrom_integral_threshold = 1e-9
+pyscf_angstrom_integral_threshold = 5e-7
 
-def test_two_center_all_angular_spherical_h2():
+def test_three_center_all_angular_spherical_h2():
     molecule = read_xyz_content(
         """2
 
@@ -96,7 +96,7 @@ def test_two_center_all_angular_spherical_h2():
 
     np.testing.assert_allclose(test_J3c, ref_J3c, rtol = 0, atol = pyscf_bohr_integral_threshold)
 
-def test_two_center_all_angular_cartesian_h2():
+def test_three_center_all_angular_cartesian_h2():
     molecule = read_xyz_content(
         """2
 
@@ -175,182 +175,252 @@ def test_two_center_all_angular_cartesian_h2():
 
     np.testing.assert_allclose(test_J3c, ref_J3c, rtol = 0, atol = pyscf_bohr_integral_threshold)
 
-# def test_two_center_hof():
-#     molecule = read_xyz_content(
-#         """3
+def test_three_center_hof():
+    molecule = read_xyz_content(
+        """3
 
-#         H -1 0.1 0
-#         O 0 0 0
-#         F 0.5 0.6 0.7
-#         """
-#     )
-#     basis_set = read_basis("def2-svp-rifit")
-#     attach_auxiliary_basis_to_molecule(molecule, basis_set)
-#     pair_data = form_auxiliary_pair_data(molecule)
-#     test_J2c = two_center(pair_data, molecule)
+        H -1 0.1 0
+        O 0 0 0
+        F 0.5 0.6 0.7
+        """
+    )
+    basis_set = read_basis("sto-3g")
+    attach_basis_to_molecule(molecule, basis_set)
+    pair_data = form_pair_data(molecule, 1e-14)
+    auxiliary_basis_set = read_basis("def2-svp-rifit")
+    attach_auxiliary_basis_to_molecule(molecule, auxiliary_basis_set)
+    auxiliary_pair_data = form_auxiliary_pair_data(molecule)
+    test_J3c = three_center(pair_data, auxiliary_pair_data, molecule)
 
-#     ref_J2c = np.loadtxt(reference_directory / 'reference_two_center_hof_data.txt')
+    ref_J3c = np.load(reference_directory / 'reference_three_center_hof_data.npz')["J3c"]
 
-#     np.testing.assert_allclose(test_J2c, ref_J2c, rtol = 0, atol = pyscf_angstrom_integral_threshold)
+    np.testing.assert_allclose(test_J3c, ref_J3c, rtol = 0, atol = pyscf_angstrom_integral_threshold)
 
-# def test_two_center_hof_distorted():
-#     molecule = read_xyz_content(
-#         """3
+def test_three_center_hof_distorted():
+    molecule = read_xyz_content(
+        """3
 
-#         H -1 0.1 0
-#         O 0 0 0
-#         F 0.5 0.6 2.7
-#         """
-#     )
-#     basis_set = read_basis("def2-svp-rifit")
-#     attach_auxiliary_basis_to_molecule(molecule, basis_set)
-#     pair_data = form_auxiliary_pair_data(molecule)
-#     test_J2c = two_center(pair_data, molecule)
+        H -1 0.1 0
+        O 0 0 0
+        F 0.5 0.6 2.7
+        """
+    )
+    basis_set = read_basis("sto-3g")
+    attach_basis_to_molecule(molecule, basis_set)
+    pair_data = form_pair_data(molecule, 1e-14)
+    auxiliary_basis_set = read_basis("def2-svp-rifit")
+    attach_auxiliary_basis_to_molecule(molecule, auxiliary_basis_set)
+    auxiliary_pair_data = form_auxiliary_pair_data(molecule)
+    test_J3c = three_center(pair_data, auxiliary_pair_data, molecule)
 
-#     ref_J2c = np.loadtxt(reference_directory / 'reference_two_center_hof_distorted_data.txt')
+    ref_J3c = np.load(reference_directory / 'reference_three_center_hof_distorted_data.npz')["J3c"]
 
-#     np.testing.assert_allclose(test_J2c, ref_J2c, rtol = 0, atol = pyscf_angstrom_integral_threshold)
+    np.testing.assert_allclose(test_J3c, ref_J3c, rtol = 0, atol = pyscf_angstrom_integral_threshold)
 
-# def test_two_center_all_angular_spherical_h2_omega():
-#     molecule = read_xyz_content(
-#         """2
+def test_three_center_all_angular_spherical_h2_omega():
+    molecule = read_xyz_content(
+        """2
 
-#         H 0 0 0
-#         H 1.0 1.3 1.55
-#         """,
-#         unit = LengthUnits.BOHR
-#     )
-#     molecule.spherical_basis = True
-#     basis_set = read_basis_content("""
-#         H     0
-#         S    2   1.00
-#             1.0    1.0
-#             0.5    1.0
-#         P    2   1.00
-#             1.2    1.0
-#             0.6    1.0
-#         D    2   1.00
-#             1.4    1.0
-#             0.7    1.0
-#         F    2   1.00
-#             1.6    1.0
-#             0.8    1.0
-#         G    2   1.00
-#             1.8    1.0
-#             0.9    1.0
-#         H    2   1.00
-#             2.0    1.0
-#             1.0    1.0
-#         I    2   1.00
-#             2.2    1.0
-#             1.1    1.0
-#         K    2   1.00
-#             1.9    1.0
-#             0.9    1.0
-#         L    2   1.00
-#             1.8    1.0
-#             0.8    1.0
-#         M    2   1.00
-#             1.7    1.0
-#             0.7    1.0
-#         ****
-#         """)
-#     attach_auxiliary_basis_to_molecule(molecule, basis_set)
-#     pair_data = form_auxiliary_pair_data(molecule)
-#     omega = 0.3
-#     test_J2c = two_center(pair_data, molecule, omega)
+        H 0 0 0
+        H 1.0 1.3 1.55
+        """,
+        unit = LengthUnits.BOHR
+    )
+    molecule.spherical_basis = True
+    basis_set = read_basis_content("""
+        H     0
+        S    2   1.00
+            2.0    0.5
+            0.5    1.0
+        P    2   1.00
+            2.05    0.5
+            0.45    1.0
+        D    2   1.00
+            2.15    0.5
+            0.40    1.0
+        F    2   1.00
+            2.25    0.5
+            0.35    1.0
+        G    2   1.00
+            2.35    0.5
+            0.30    1.0
+        H    2   1.00
+            2.45    0.5
+            0.25    1.0
+        I    2   1.00
+            2.55    0.5
+            0.20    1.0
+        ****
+    """)
+    auxiliary_basis_set = read_basis_content("""
+        H     0
+        S    2   1.00
+            1.0    1.0
+            0.5    1.0
+        P    2   1.00
+            1.2    1.0
+            0.6    1.0
+        D    2   1.00
+            1.4    1.0
+            0.7    1.0
+        F    2   1.00
+            1.6    1.0
+            0.8    1.0
+        G    2   1.00
+            1.8    1.0
+            0.9    1.0
+        H    2   1.00
+            2.0    1.0
+            1.0    1.0
+        I    2   1.00
+            2.2    1.0
+            1.1    1.0
+        K    2   1.00
+            1.9    1.0
+            0.9    1.0
+        L    2   1.00
+            1.8    1.0
+            0.8    1.0
+        M    2   1.00
+            1.7    1.0
+            0.7    1.0
+        ****
+        """)
+    attach_basis_to_molecule(molecule, basis_set)
+    pair_data = form_pair_data(molecule, 1e-14)
+    attach_auxiliary_basis_to_molecule(molecule, auxiliary_basis_set)
+    auxiliary_pair_data = form_auxiliary_pair_data(molecule)
 
-#     ref_J2c = np.loadtxt(reference_directory / 'reference_two_center_all_angular_spherical_h2_omega_data.txt')
+    omega = 0.2
+    test_J3c = three_center(pair_data, auxiliary_pair_data, molecule, omega)
 
-#     np.testing.assert_allclose(test_J2c, ref_J2c, rtol = 0, atol = pyscf_bohr_integral_threshold)
+    ref_J3c = np.load(reference_directory / 'reference_three_center_all_angular_spherical_h2_omega_data.npz')["J3c"]
 
-# def test_two_center_all_angular_cartesian_h2_omega():
-#     molecule = read_xyz_content(
-#         """2
+    np.testing.assert_allclose(test_J3c, ref_J3c, rtol = 0, atol = pyscf_bohr_integral_threshold)
 
-#         H 0 0 0
-#         H 1.0 1.3 1.55
-#         """,
-#         unit = LengthUnits.BOHR
-#     )
-#     molecule.spherical_basis = False
-#     basis_set = read_basis_content("""
-#         H     0
-#         S    2   1.00
-#             1.0    1.0
-#             0.5    1.0
-#         P    2   1.00
-#             1.2    1.0
-#             0.6    1.0
-#         D    2   1.00
-#             1.4    1.0
-#             0.7    1.0
-#         F    2   1.00
-#             1.6    1.0
-#             0.8    1.0
-#         G    2   1.00
-#             1.8    1.0
-#             0.9    1.0
-#         H    2   1.00
-#             2.0    1.0
-#             1.0    1.0
-#         I    2   1.00
-#             2.2    1.0
-#             1.1    1.0
-#         K    2   1.00
-#             1.9    1.0
-#             0.9    1.0
-#         L    2   1.00
-#             1.8    1.0
-#             0.8    1.0
-#         M    2   1.00
-#             1.7    1.0
-#             0.7    1.0
-#         ****
-#         """)
-#     attach_auxiliary_basis_to_molecule(molecule, basis_set)
-#     pair_data = form_auxiliary_pair_data(molecule)
-#     omega = 0.3
-#     test_J2c = two_center(pair_data, molecule, omega)
+def test_three_center_all_angular_cartesian_h2_omega():
+    molecule = read_xyz_content(
+        """2
 
-#     ref_J2c = np.loadtxt(reference_directory / 'reference_two_center_all_angular_cartesian_h2_omega_data.txt')
+        H 0 0 0
+        H 1.0 1.3 1.55
+        """,
+        unit = LengthUnits.BOHR
+    )
+    molecule.spherical_basis = False
+    basis_set = read_basis_content("""
+        H     0
+        S    2   1.00
+            2.0    0.5
+            0.5    1.0
+        P    2   1.00
+            2.05    0.5
+            0.45    1.0
+        D    2   1.00
+            2.15    0.5
+            0.40    1.0
+        F    2   1.00
+            2.25    0.5
+            0.35    1.0
+        G    2   1.00
+            2.35    0.5
+            0.30    1.0
+        H    2   1.00
+            2.45    0.5
+            0.25    1.0
+        I    2   1.00
+            2.55    0.5
+            0.20    1.0
+        ****
+    """)
+    auxiliary_basis_set = read_basis_content("""
+        H     0
+        S    2   1.00
+            1.0    1.0
+            0.5    1.0
+        P    2   1.00
+            1.2    1.0
+            0.6    1.0
+        D    2   1.00
+            1.4    1.0
+            0.7    1.0
+        F    2   1.00
+            1.6    1.0
+            0.8    1.0
+        G    2   1.00
+            1.8    1.0
+            0.9    1.0
+        H    2   1.00
+            2.0    1.0
+            1.0    1.0
+        I    2   1.00
+            2.2    1.0
+            1.1    1.0
+        K    2   1.00
+            1.9    1.0
+            0.9    1.0
+        L    2   1.00
+            1.8    1.0
+            0.8    1.0
+        M    2   1.00
+            1.7    1.0
+            0.7    1.0
+        ****
+        """)
+    attach_basis_to_molecule(molecule, basis_set)
+    pair_data = form_pair_data(molecule, 1e-14)
+    attach_auxiliary_basis_to_molecule(molecule, auxiliary_basis_set)
+    auxiliary_pair_data = form_auxiliary_pair_data(molecule)
 
-#     np.testing.assert_allclose(test_J2c, ref_J2c, rtol = 0, atol = pyscf_bohr_integral_threshold)
+    omega = 0.2
+    test_J3c = three_center(pair_data, auxiliary_pair_data, molecule, omega)
 
-# def test_two_center_hof_omega():
-#     molecule = read_xyz_content(
-#         """3
+    ref_J3c = np.load(reference_directory / 'reference_three_center_all_angular_cartesian_h2_omega_data.npz')["J3c"]
 
-#         H -1 0.1 0
-#         O 0 0 0
-#         F 0.5 0.6 0.7
-#         """
-#     )
-#     basis_set = read_basis("def2-svp-rifit")
-#     attach_auxiliary_basis_to_molecule(molecule, basis_set)
-#     pair_data = form_auxiliary_pair_data(molecule)
-#     omega = 0.3
-#     test_J2c = two_center(pair_data, molecule, omega)
+    np.testing.assert_allclose(test_J3c, ref_J3c, rtol = 0, atol = pyscf_bohr_integral_threshold)
 
-#     ref_J2c = np.loadtxt(reference_directory / 'reference_two_center_hof_omega_data.txt')
+def test_three_center_hof_omega():
+    molecule = read_xyz_content(
+        """3
 
-#     np.testing.assert_allclose(test_J2c, ref_J2c, rtol = 0, atol = pyscf_angstrom_integral_threshold)
+        H -1 0.1 0
+        O 0 0 0
+        F 0.5 0.6 0.7
+        """
+    )
+    basis_set = read_basis("sto-3g")
+    attach_basis_to_molecule(molecule, basis_set)
+    pair_data = form_pair_data(molecule, 1e-14)
+    auxiliary_basis_set = read_basis("def2-svp-rifit")
+    attach_auxiliary_basis_to_molecule(molecule, auxiliary_basis_set)
+    auxiliary_pair_data = form_auxiliary_pair_data(molecule)
 
-# def test_two_center_hof_distorted_omega():
-#     molecule = read_xyz_content(
-#         """3
+    omega = 0.2
+    test_J3c = three_center(pair_data, auxiliary_pair_data, molecule, omega)
 
-#         H -1 0.1 0
-#         O 0 0 0
-#         F 0.5 0.6 2.7
-#         """
-#     )
-#     basis_set = read_basis("def2-svp-rifit")
-#     attach_auxiliary_basis_to_molecule(molecule, basis_set)
-#     pair_data = form_auxiliary_pair_data(molecule)
-#     omega = 0.3
-#     test_J2c = two_center(pair_data, molecule, omega)
+    ref_J3c = np.load(reference_directory / 'reference_three_center_hof_omega_data.npz')["J3c"]
 
-#     ref_J2c = np.loadtxt(reference_directory / 'reference_two_center_hof_distorted_omega_data.txt')
+    np.testing.assert_allclose(test_J3c, ref_J3c, rtol = 0, atol = pyscf_angstrom_integral_threshold)
 
-#     np.testing.assert_allclose(test_J2c, ref_J2c, rtol = 0, atol = pyscf_angstrom_integral_threshold)
+def test_three_center_hof_distorted_omega():
+    molecule = read_xyz_content(
+        """3
+
+        H -1 0.1 0
+        O 0 0 0
+        F 0.5 0.6 2.7
+        """
+    )
+    basis_set = read_basis("sto-3g")
+    attach_basis_to_molecule(molecule, basis_set)
+    pair_data = form_pair_data(molecule, 1e-14)
+    auxiliary_basis_set = read_basis("def2-svp-rifit")
+    attach_auxiliary_basis_to_molecule(molecule, auxiliary_basis_set)
+    auxiliary_pair_data = form_auxiliary_pair_data(molecule)
+
+    omega = 0.2
+    test_J3c = three_center(pair_data, auxiliary_pair_data, molecule, omega)
+
+    ref_J3c = np.load(reference_directory / 'reference_three_center_hof_distorted_omega_data.npz')["J3c"]
+
+    np.testing.assert_allclose(test_J3c, ref_J3c, rtol = 0, atol = pyscf_angstrom_integral_threshold)
